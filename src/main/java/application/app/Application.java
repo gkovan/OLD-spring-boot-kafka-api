6bc5d29d9915fb4d63cfe7423117ee9a0d25e06f
gkovan <NH43863@hertz.net>
2020-06-03 00:45:52 -0400

    json example working

--- src/main/java/application/app/Application.java
+++ src/main/java/application/app/Application.java
@@ -12,9 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-@ServletComponentScan({"com.ibm.orderskafka.*", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*",})
+@ServletComponentScan({"com.ibm.orderskafka.*", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*", "com.ibm.kafkastream.json.*"})
 @SpringBootApplication
-@ComponentScan({"com.ibm.hello.*","com.ibm.simplekafka.*", "com.ibm.orderskafka.*", "com.ibm.config", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
+@ComponentScan({"com.ibm.hello.*","com.ibm.simplekafka.*", "com.ibm.orderskafka.*", "com.ibm.config", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*", "com.ibm.kafkastream.json.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
 public class Application extends SpringBootServletInitializer {
     @Autowired
     Environment environment;
